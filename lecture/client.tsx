75653d35bf325d449df706b2ae27353b62054534
GitHappyGod <chami0205@gmail.com>
2022-12-09 12:51:40 +0900

    2. WordRelay useCallback

--- lecture/client.tsx
+++ lecture/client.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDom from 'react-dom';
 
-import GuGuDan from './GuGuDan';
 import WordRelay from './WordRelay';
 
-// ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
-ReactDOM.render(<WordRelay />, document.querySelector('#root'));
+ReactDom.render(<WordRelay />, document.querySelector('#root'));
