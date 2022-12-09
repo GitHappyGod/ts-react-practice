b56727e8ecc12b9e5f1dc7716714c19f05f48dda
GitHappyGod <chami0205@gmail.com>
2022-12-09 09:54:08 +0900

    1. GuGuDan

--- lecture/client.tsx
+++ lecture/client.tsx
@@ -2,5 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import GuGuDan from './GuGuDan';
+import WordRelay from './WordRelay';
 
-ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
+// ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
+ReactDOM.render(<WordRelay />, document.querySelector('#root'));
